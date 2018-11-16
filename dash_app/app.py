6644a77b2cc2f8a602accf70c6ab6ee02b502754
Blair Thurman <blairdev1023@gmail.com>
2018-11-15 23:33:55 -0700

    churn EDA v1

--- dash_app/app.py
+++ dash_app/app.py
@@ -5,13 +5,14 @@ import dash_core_components as dcc
 from dash.dependencies import Input, Output
 import plotly.graph_objs as go
 import pandas as pd
-
-import dash
+import colorlover as cl
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 ################################# Pandas Stuff #################################
 df = pd.read_csv('../data/telco_churn_clean.csv')
+df_churn = df[df['Churn'] == 'Yes']
+df_no_churn = df[df['Churn'] == 'No']
 
 ################################ Common Labels #################################
 # columns with less than 5 unique values, all others are non categorical
@@ -35,7 +36,8 @@ app.layout = html.Div([
             'float': 'left',
             'display': 'inline-block',
             'height': 500
-        }),
+        },
+        config={'displayModeBar': False}),
     # Dropdown and Pie Chart
     html.Div([
         dcc.Dropdown(
@@ -43,7 +45,7 @@ app.layout = html.Div([
             options=[{'label': i, 'value': i} for i in categorical_cols],
             value='TechSupport'
         ),
-        dcc.Graph(id='feature-pie')
+        dcc.Graph(id='feature-pie', config={'displayModeBar': False})
     ], style={
         'width': '50%',
         'float': 'left',
@@ -55,12 +57,22 @@ app.layout = html.Div([
 @app.callback(Output('feature-bar', 'figure'),
               [Input('feature-dropdown', 'value')])
 def display_bar(col):
-    trace_series = df[col].value_counts().sort_index()
-    data = [go.Trace(
-        x=trace_series.index,
-        y=trace_series.values,
-        type='bar'
-    )]
+    churn_series = df_churn[col].value_counts().sort_index()
+    no_churn_series = df_no_churn[col].value_counts().sort_index()
+
+    data = []
+    data.append(go.Bar(
+        x=churn_series.index,
+        y=churn_series.values,
+        name='Churn',
+        marker={'color': 'red'}
+    ))
+    data.append(go.Bar(
+        x=no_churn_series.index,
+        y=no_churn_series.values,
+        name='No Churn',
+        marker={'color': 'blue'}
+    ))
 
     layout = go.Layout(
         title=f'{col} Visualization'
@@ -70,12 +82,39 @@ def display_bar(col):
 @app.callback(Output('feature-pie', 'figure'),
               [Input('feature-dropdown', 'value')])
 def display_pie(col):
-    trace_series = df[col].value_counts().sort_index() / len(df)
-    data = [go.Pie(
-        labels=trace_series.index,
-        values=trace_series.values,
-        hole=0.5
-    )]
+    n_churn = len(df_churn)
+    n_no_churn = len(df_no_churn)
+    churn_series = df_churn[col].value_counts().sort_index() / n_churn
+    no_churn_series = df_no_churn[col].value_counts().sort_index() / n_no_churn
+    n_features = len(churn_series.index)
+
+    # The whole plus one minus one thing is to skip the ultra white first color
+    colors = cl.scales[str(n_features+1)]['seq']['Greens'][1:]
+
+    # data, churn pie
+    data = []
+    data.append(go.Pie(
+        labels=churn_series.index,
+        values=churn_series.values,
+        hole=0.65,
+        domain={'x': [0, 1], 'y': [0, 1]},
+        marker={
+            'line': {'color': 'white', 'width': 2},
+            'colors': colors
+        },
+        sort=False
+    ))
+    # data, no churn pie
+    data.append(go.Pie(
+        labels=no_churn_series.index,
+        values=no_churn_series.values,
+        domain={'x': [0.2, 0.8], 'y': [0.2, 0.8]},
+        marker={
+            'line': {'color': 'white', 'width': 2},
+            'colors': colors
+        },
+        sort=False
+    ))
 
     layout = go.Layout(
         title=f'{col} Visualization'
